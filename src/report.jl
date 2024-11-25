fdb4aa8f92032c3a1708b394638faa9ae1c47900
João Martins Filho <joaomfilho1995@gmail.com>
2024-11-24 23:52:52 -0300

    create save results and first graph

--- src/report.jl
+++ src/report.jl
@@ -7,15 +7,25 @@ using Plots
 
 Gera um gráfico de tempos de resposta.
 """
-function generate_report(results::Dict)
+function generate_report(results::Dict, file_name::String="grafico.png")
     times = results["all_times"]
-    plot(
+    
+    if isempty(times)
+        println("Erro: Nenhum dado de tempo encontrado.")
+        return
+    end
+    
+    p = plot(
         1:length(times), times,
         xlabel="Requisição", ylabel="Tempo (s)",
         title="Desempenho do Endpoint",
         legend=false
     )
+
+    savefig(p, file_name)
+    println("Gráfico salvo em: $file_name")
 end
+
 
 export generate_report
 
