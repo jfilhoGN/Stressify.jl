4d153ecbea29833f21d99d00444822803d9d9242
João Martins Filho <joaomfilho1995@gmail.com>
2024-12-07 16:41:30 -0300

    adjusts in actions and coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,22 @@ using Coverage
 using Test
 using Stressify
 
-Coverage.@coverage begin
-    include("test_utils.jl")
-    include("test_report.jl")
-    include("test_core.jl") 
+# Inclua os arquivos de teste
+include("test_utils.jl")
+include("test_report.jl")
+include("test_core.jl")
+
+# Obtenha a cobertura de código
+coverage_data = @coverage begin
+    # Execute seus testes
+    @testset "Stressify Tests" begin
+        include("test_utils.jl")
+        include("test_report.jl")
+        include("test_core.jl")
+    end
 end
 
-lcov_report = joinpath(pwd(), "coverage.lcov")
-println("Saving coverage report to $lcov_report")
-Coverage.lcov_write(lcov_report)
+# Gere o relatório de cobertura em formato LCOV
+lcov_file = joinpath(pwd(), "coverage.lcov")
+write(lcov_file, LCOV.writeLcov(coverage_data))
+println("Cobertura gerada em $lcov_file")
