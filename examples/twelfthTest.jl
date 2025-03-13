a6681871e0e8f3eac6b059b9cdaa5cfe1dac7325
João Martins Filho <joaomfilho1995@gmail.com>
2025-03-13 00:43:00 -0300

    create cli outputs to integration with grafana and influxDB

--- examples/twelfthTest.jl
+++ examples/twelfthTest.jl
@@ -1,7 +1,13 @@
 using Pkg: Pkg
 Pkg.activate(".")
+Pkg.instantiate()
 using Stressify
 
+"""
+Execute in CLI
+export INFLUXDB_TOKEN="your_token"
+julia examples/twelfthTest.jl --output=influxdb --influxdb-url="http://localhost:8086" --influxdb-bucket="xxxxx" --influxdb-org="xxxxxx"
+"""
 #execute for the one VU for one iteration
 Stressify.options(
 	vus = 1,
